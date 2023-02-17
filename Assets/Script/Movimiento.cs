46f709561a4c1ecb3b80c4bd3e7ce332b8b62b3d
Gerardo <A01641788@tec.mx>
2023-02-17 08:59:18 -0600

    Push inicial

---
+++ Assets/Script/Movimiento.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    //ciclo de vida / lifecycle
+    //existe metodos que se invocan en momento especificos de la vida del script
+
+    // Start is called before the first frame update
+    
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate(){
+
+    }
+}
